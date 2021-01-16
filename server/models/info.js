80bce607ba4cd87e79d2b99b8e93be339d8bc086
daniel_232526 <dan.iban232526@gmail.com>
2021-01-15 23:46:19 -0500

    Correcion 2

--- server/models/info.js
+++ server/models/info.js
@@ -11,6 +11,17 @@ let informacion = new Schema({
         type: String,
         required: [true, "La fecha es obligatoria"]
     },
+    anio: {
+        type: String,
+        required: [true, 'El anio es obligatorio']
+    },
+    mes: {
+        type: String,
+        required: [true, 'El mes es obligatorio']
+    },
+    dia: {
+        type: String,
+        required: [true, 'El dia es obligatorio'],
     hora: {
         type: String,
         required: [true, "La hora es oblgatorio"]
